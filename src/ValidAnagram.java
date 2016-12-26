2a6248771948e6ebf598026497f5786137ae39ce
pengshuang <pengshuang92@163.com>
2016-12-26 15:52:20 +0800

    2/291

--- src/ValidAnagram.java
+++ src/ValidAnagram.java
@@ -3,6 +3,19 @@
  */
 public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
-
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            count[(int) s.charAt(i)]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            count[(int) t.charAt(i)]--;
+            if (count[(int) t.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
