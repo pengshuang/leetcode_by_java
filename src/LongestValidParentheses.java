1a83f064c99bf22b46cd9745a637638241f64ae5
pengshuang <pengshuang92@163.com>
2017-01-06 16:32:59 +0800

    30/291

--- src/LongestValidParentheses.java
+++ src/LongestValidParentheses.java
@@ -7,24 +7,17 @@ import java.util.Stack;
 
 public class LongestValidParentheses {
     public int longestValidParentheses(String s) {
-        Stack<Integer> stack = new Stack<>();
-        int max = 0;
-        int last = -1; //保存最后一个不匹配的 ')' 的下标
-        char c;
-        for(int i=0;i<s.length();i++){
-            c=s.charAt(i);
-            if(c == '('){
-                stack.push(i);
-            }else if(c == ')'){
-                if(stack.isEmpty()){
-                    last = i; //更新最后一个不匹配的 ')'
-                } else {
+        Stack<Integer> stack = new Stack<Integer>();
+        int max=0;
+        int left = -1;
+        for(int j=0;j<s.length();j++){
+            if(s.charAt(j)=='(') stack.push(j);
+            else {
+                if (stack.isEmpty()) left=j;
+                else{
                     stack.pop();
-                    if(stack.isEmpty()){
-                        max = Math.max(max, i - last);
-                    } else {
-                        max = Math.max(max, i - stack.peek());
-                    }
+                    if(stack.isEmpty()) max=Math.max(max,j-left);
+                    else max=Math.max(max,j-stack.peek());
                 }
             }
         }
