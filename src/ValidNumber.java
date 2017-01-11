9d14098d4b9f6377b32385e2031501029641376d
pengshuang <pengshuang92@163.com>
2017-01-11 09:08:38 +0800

    45/291

---
+++ src/ValidNumber.java
@@ -0,0 +1,37 @@
+/**
+ * Created by pengshuang on 17/1/11.
+ */
+public class ValidNumber {
+    public boolean isNumber(String s) {
+        s = s.trim();
+        if (s.length() < 1 || s.charAt(s.length()-1) == 'e') return false;
+        String[] sp = s.split("e");
+        if (sp.length > 2 || sp.length < 1) return false;
+        if (!validNumberic(sp[0], false)) return false;
+        if (sp.length == 1 || (sp.length == 2 && validNumberic(sp[1], true))) return true;
+        return false;
+    }
+    private boolean validNumberic(String s, boolean integer){
+        int index = 0;
+        if (s.length() < 1 || s.equals("."))
+            return false;
+        if (s.charAt(0) == '+' || s.charAt(0) == '-')
+            index++;
+        if (index == s.length())
+            return false;
+        char c;
+        boolean hasPoint = false;
+        boolean hasNum = false;
+        for(;index < s.length();index++){
+            c = s.charAt(index);
+            if(c == '.'){
+                if(!hasPoint && !integer) hasPoint = true;
+                else return false;
+                continue;
+            }
+            if(c < '0' || c > '9') return false;
+            hasNum = true;
+        }
+        return hasNum;
+    }
+}
