f1bd1ddc79b2824885d139b5531640d4b8514663
pengshuang <pengshuang92@163.com>
2017-01-14 09:56:16 +0800

    53/291

---
+++ src/UniquePathsII.java
@@ -0,0 +1,41 @@
+/**
+ * Created by pengshuang on 17/1/14.
+ */
+public class UniquePathsII {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0){
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] paths = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            if(obstacleGrid[i][0] != 1) {
+                paths[i][0] = 1;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(obstacleGrid[0][i] != 1) {
+                paths[0][i] = 1;
+            } else {
+                break;
+            }
+        }
+
+        for(int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if(obstacleGrid[i][j] != 1) {
+                    paths[i][j] = paths[i-1][j] + paths[i][j-1];
+                } else {
+                    paths[i][j] = 0;
+                }
+            }
+        }
+
+        return paths[m-1][n-1];
+    }
+}
