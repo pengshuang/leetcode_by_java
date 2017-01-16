a3a5cba4412d2f491e43b9fb65c80afc95ec03ff
pengshuang <pengshuang92@163.com>
2017-01-16 09:15:07 +0800

    57/291

---
+++ src/SumRoottoLeafNumbers.java
@@ -0,0 +1,24 @@
+/**
+ * Created by pengshuang on 17/1/16.
+ */
+public class SumRoottoLeafNumbers {
+    private int sum;
+    public int sumNumbers(TreeNode root) {
+        recursivePath(root, 0);
+        return sum;
+    }
+    private void recursivePath(TreeNode root, int num) {
+        if (root == null)
+            return;
+        num = num * 10 + root.val;
+        if (root.left != null) {
+            recursivePath(root.left, num);
+        }
+        if (root.right != null) {
+            recursivePath(root.right, num);
+        }
+        if (root.left == null && root.right == null) {
+            sum += num;
+        }
+    }
+}
