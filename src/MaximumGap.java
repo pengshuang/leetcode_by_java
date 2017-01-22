a7c73355956601719c10367e3281995c8b30a22d
pengshuang <pengshuang92@163.com>
2017-01-22 10:53:01 +0800

    72/291

--- src/MaximumGap.java
+++ src/MaximumGap.java
@@ -1,56 +1,57 @@
+import java.util.Arrays;
+
 /**
  * Created by pengshuang on 17/1/22.
+ 假设有N个元素A到B。
+
+ 那么最大差值不会小于ceiling[(B - A) / (N - 1)]
+
+ 令bucket（桶）的大小len = ceiling[(B - A) / (N - 1)]，则最多会有(B - A) / len + 1个桶
+
+ 对于数组中的任意整数K，很容易通过算式loc = (K - A) / len找出其桶的位置，然后维护每一个桶的最大值和最小值
+
+ 由于同一个桶内的元素之间的差值至多为len - 1，因此最终答案不会从同一个桶中选择。
+
+ 对于每一个非空的桶p，找出下一个非空的桶q，则q.min - p.max可能就是备选答案。返回所有这些可能值中的最大值。
  */
 public class MaximumGap {
-
-    private int min(int a, int b) {
-        if (a==-1) return b;
-        else
-        if (b==-1) return a;
-        else
-        if (a<b) return a;
-        else return b;
-    }
-
-    private int max(int a, int b) {
-        if (a==-1) return b;
-        else
-        if (b==-1) return a;
-        else
-        if (a>b) return a;
-        else return b;
-    }
-
-    public int maximumGap(int[] nums) {
-        if (nums.length < 2)
+    public int maximumGap(int[] num) {
+        if (num == null || num.length < 2)
             return 0;
-        int minNum = -1, maxNum = -1, n = nums.length;
-        for (int i=0; i<n; ++i) {
-            minNum = min(nums[i], minNum);
-            maxNum = max(nums[i], maxNum);
+        // get the max and min value of the array
+        int min = num[0];
+        int max = num[0];
+        for (int i:num) {
+            min = Math.min(min, i);
+            max = Math.max(max, i);
         }
-        if (maxNum==minNum) return 0;
-        double average = (maxNum-minNum) * 1.0 / (n - 1);
-        if (average==0) ++average;
-        int[] localMin = new int[n];
-        int[] localMax = new int[n];
-        for (int i=0; i<n; ++i) {
-            localMin[i] = -1;
-            localMax[i] = -1;
+        // the minimum possibale gap, ceiling of the integer division
+        int gap = (int)Math.ceil((double)(max - min)/(num.length - 1));
+        int[] bucketsMIN = new int[num.length - 1]; // store the min value in that bucket
+        int[] bucketsMAX = new int[num.length - 1]; // store the max value in that bucket
+        Arrays.fill(bucketsMIN, Integer.MAX_VALUE);
+        Arrays.fill(bucketsMAX, Integer.MIN_VALUE);
+        // put numbers into buckets
+        for (int i:num) {
+            if (i == min || i == max)
+                continue;
+            int idx = (i - min) / gap; // index of the right position in the buckets
+            bucketsMIN[idx] = Math.min(i, bucketsMIN[idx]);
+            bucketsMAX[idx] = Math.max(i, bucketsMAX[idx]);
         }
-        for (int i=0; i<n; ++i) {
-            int t = (int)((nums[i]-minNum) / average);
-            localMin[t] = min(localMin[t], nums[i]);
-            localMax[t] = max(localMax[t], nums[i]);
+        // scan the buckets for the max gap
+        int maxGap = Integer.MIN_VALUE;
+        int previous = min;
+        for (int i = 0; i < num.length - 1; i++) {
+            if (bucketsMIN[i] == Integer.MAX_VALUE && bucketsMAX[i] == Integer.MIN_VALUE)
+                // empty bucket
+                continue;
+            // min value minus the previous value is the current gap
+            maxGap = Math.max(maxGap, bucketsMIN[i] - previous);
+            // update previous bucket value
+            previous = bucketsMAX[i];
         }
-        int ans = (int)average, left = 0, right = 1;
-        while (left<n-1) {
-            while (right<n && localMin[right]==-1) ++right;
-            if (right>=n) break;
-            ans = max(ans, localMin[right]-localMax[left]);
-            left = right;
-            ++right;
-        }
-        return ans;
+        maxGap = Math.max(maxGap, max - previous); // updata the final max value gap
+        return maxGap;
     }
 }
