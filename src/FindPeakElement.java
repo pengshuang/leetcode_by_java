fc3cfebb2b59055ad8c4154a018840b9c9a27164
pengshuang <pengshuang92@163.com>
2017-01-22 11:32:36 +0800

    73/291

---
+++ src/FindPeakElement.java
@@ -0,0 +1,15 @@
+
+/**
+ * Created by pengshuang on 17/1/22.
+ */
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+        return right;
+    }
+}
