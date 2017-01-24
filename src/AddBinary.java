3a798108a9f9df70b0c1c6af4a3233fad409ac89
pengshuang <pengshuang92@163.com>
2017-01-24 10:06:58 +0800

    复习

--- src/AddBinary.java
+++ src/AddBinary.java
@@ -32,4 +32,33 @@ public class AddBinary {
             rst = "1" + rst;
         return rst;
     }
+
+    public String addBinary2(String a, String b) {
+        int len1 = a.length() - 1;
+        int len2 = b.length() - 1;
+        int carry = 0;
+        String rst = "";
+        int x, y;
+        while (len1 >= 0 || len2 >= 0){
+            if (len1 >= 0)
+                x = (int)(a.charAt(len1) - '0');
+            else
+                x = 0;
+            if (len2 >= 0)
+                y = (int)(b.charAt(len2) - '0');
+            else
+                y = 0;
+            if ((x + y + carry) % 2 == 0)
+                rst = '0' + rst;
+            else
+                rst = '1' + rst;
+            carry = (x + y + carry) / 2;
+            len1 -= 1;
+            len2 -= 1;
+        }
+        if (carry == 1){
+            rst = '1' + rst;
+        }
+        return rst;
+    }
 }
