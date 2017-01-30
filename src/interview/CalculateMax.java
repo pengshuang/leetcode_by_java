f94c271f382eef825f36ff314b826d7731180c32
pengshuang <pengshuang92@163.com>
2017-01-30 10:33:36 +0800

    81/300

--- src/interview/CalculateMax.java
+++ src/interview/CalculateMax.java
@@ -2,7 +2,12 @@ package interview;
 
 /**
  * Created by pengshuang on 17/1/30.
+ * firstBuy表示第一次买入的最大收益，因为是买入所以是负值；
+ * firstSell表示第一次卖出的最大收益；
+ * secondBuy表示第二次买入的最大收益；
+ * secondSell表示第二次卖出的最大收益；
  */
+
 public class CalculateMax {
     public int calculateMax(int[] prices) {
         int firstBuy = Integer.MIN_VALUE, firstSell = 0;
