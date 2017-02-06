d4f4e11b8166ef9107023da684afeebde0654463
pengshuang <pengshuang92@163.com>
2017-02-06 17:18:42 +0800

    89/300

---
+++ src/MergeKLists.java
@@ -0,0 +1,47 @@
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.List;
+
+/**
+ * Created by pengshuang on 17/2/6.
+ */
+public class MergeKLists {
+    public ListNode mergeKLists(List<ListNode> lists) {
+        if (lists.size() == 0) {
+            return null;
+        }
+        return mergeHelper(lists, 0, lists.size() - 1);
+    }
+
+    private ListNode mergeHelper(List<ListNode> lists, int start, int end) {
+        if (start == end) {
+            return lists.get(start);
+        }
+        int mid = start + (end - start) >> 1;
+        ListNode left = mergeHelper(lists, start, mid);
+        ListNode right = mergeHelper(lists, mid + 1, end);
+        return mergeTwoLists(left, right);
+    }
+
+    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                tail = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                tail = list2;
+                list2 = list2.next;
+            }
+        }
+        if (list1 != null) {
+            tail.next = list1;
+        } else {
+            tail.next = list2;
+        }
+        return dummy.next;
+    }
+}
