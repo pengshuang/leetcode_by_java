f53575b8b49900b6b20d3e65462168ef90f6dca7
pengshuang <pengshuang92@163.com>
2017-02-15 22:40:59 +0800

    93/300

---
+++ src/LongestIncreasingSubsequence.java
@@ -0,0 +1,60 @@
+/**
+ * Created by pengshuang on 17/2/15.
+ */
+public class LongestIncreasingSubsequence {
+    public int longestIncreasingSubsequence(int[] nums) {
+        int [] f = new int[nums.length];
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            f[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+            if (f[i] > max) {
+                max = f[i];
+            }
+        }
+        return max;
+    }
+
+    public int longestIncreasingSubsequence2(int[] nums) {
+        int[] minLast = new int[nums.length + 1];
+        minLast[0] = Integer.MIN_VALUE;
+        for (int i = 1; i <= nums.length; i++) {
+            minLast[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            // find the first number in minLast > nums[i]
+            int index = binarySearch(minLast, nums[i]);
+            minLast[index] = nums[i];
+        }
+
+        for (int i = nums.length; i >= 1; i--) {
+            if (minLast[i] != Integer.MAX_VALUE) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    private int binarySearch(int[] minLast, int num) {
+        int start = 0, end = minLast.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (minLast[mid] < num) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (minLast[start] > num) {
+            return start;
+        }
+        return end;
+    }
+}
