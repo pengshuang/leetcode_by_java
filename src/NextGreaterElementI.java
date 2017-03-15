86f9c55b612a1acc0e092e16ca247bac882bc813
pengshuang <pengshuang92@163.com>
2017-03-15 09:48:39 +0800

    110/300

---
+++ src/NextGreaterElementI.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * Created by pengshuang on 17/3/15.
+ */
+public class NextGreaterElementI {
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> dmap = new HashMap<>();
+
+        for (int num : nums){
+            while (!stack.isEmpty() && stack.peek() < num){
+                dmap.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        for (int i = 0; i < findNums.length; i++) {
+            if (dmap.containsKey(findNums[i]))
+                findNums[i] = dmap.get(findNums[i]);
+            else
+                findNums[i] = -1;
+        }
+       return findNums;
+    }
+}
