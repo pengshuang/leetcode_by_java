c2188160673617fd6e167992e92598b020b8be17
pengshuang <pengshuang92@163.com>
2017-05-12 11:25:29 +0800

    116/300

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -4,52 +4,18 @@
  */
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false;
-        int div = 1;
-        while((x/div) >= 10) div *= 10;
-        int mod = 10;
-        // 如果 x 小于 10
-        while (mod <= div) {
-            int left = x/div;
-            int right = x%mod;
-            if (left != right){
+        if(x<0)
+            return false;
+        int num=0;
+        int oldNum=x;
+        while(x!=0)
+        {
+            int pre=num;
+            num=num*10+x%10;
+            if(num/10!=pre)
                 return false;
-            }
-            x = (x % div)/10;
-            div /= 100;
+            x/=10;
         }
-        return true;
+        return num==oldNum;
     }
-
-    public boolean isPalindrome(String s){
-        if (s == null || s.length() == 0) {
-            return true;
-        }
-        int front = 0;
-        int end = s.length() - 1;
-        while (front < end) {
-            while (front < s.length() && !isvalid(s.charAt(front))){
-                front++;
-            }
-            if (front == s.length())
-                return true;
-            while (end >= 0 && !isvalid(s.charAt(end))){
-                end--;
-            }
-            if (end == 0)
-                return true;
-            if (Character.toLowerCase(front) == Character.toLowerCase(end)){
-                end--;
-                front++;
-            } else {
-                break;
-            }
-        }
-        return end <= front;
-    }
-
-    private boolean isvalid (char c) {
-        return Character.isLetter(c) || Character.isDigit(c);
-    }
-
 }
