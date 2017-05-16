186318c4fba5371490bde02f0deaf294721b89d9
pengshuang <pengshuang92@163.com>
2017-05-16 09:01:29 +0800

    119/300

---
+++ src/StrStr.java
@@ -0,0 +1,19 @@
+/**
+ * Created by pengshuang on 17/5/16.
+ */
+public class StrStr {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null)
+            return -1;
+        int l1 = haystack.length();
+        int l2 = needle.length();
+        for (int i = 0; i < l1 - l2 + 1; i++) {
+            int count = 0;
+            while (count < l2 && haystack.charAt(i + count) == needle.charAt(count))
+                count++;
+            if(count == l2)
+                return i;
+        }
+        return -1;
+    }
+}
