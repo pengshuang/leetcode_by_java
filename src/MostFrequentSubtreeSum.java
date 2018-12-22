123c14b07a7c97f5d1e159c037e261adf4a08a14
pengshuang <pengshuang92@163.com>
2018-12-22 12:45:32 +0800

    130/300

--- src/MostFrequentSubtreeSum.java
+++ src/MostFrequentSubtreeSum.java
@@ -5,19 +5,17 @@
 import java.util.*;
 
 public class MostFrequentSubtreeSum {
-    Map<Integer, Integer> sumToCount = new HashMap<>();
-    int maxCount = 0;
+    private Map<Integer, Integer> sumToCount = new HashMap<>();
+    private int maxCount = 0;
 
     public int[] findFrequentTreeSum(TreeNode root) {
         postOrder(root);
-
         List<Integer> res = new ArrayList<>();
         for (int key : sumToCount.keySet()) {
             if (sumToCount.get(key) == maxCount) {
                 res.add(key);
             }
         }
-
         int[] result = new int[res.size()];
         for (int i = 0; i < res.size(); i++) {
             result[i] = res.get(i);
@@ -26,15 +24,16 @@ public class MostFrequentSubtreeSum {
     }
 
     private int postOrder(TreeNode root) {
-        if (root == null) return 0;
+        if (root == null)
+            return 0;
 
         int left = postOrder(root.left);
         int right = postOrder(root.right);
 
         int sum = left + right + root.val;
         int count = sumToCount.getOrDefault(sum, 0) + 1;
-        sumToCount.put(sum, count);
 
+        sumToCount.put(sum, count);
         maxCount = Math.max(maxCount, count);
 
         return sum;
